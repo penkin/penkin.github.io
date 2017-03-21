0fb150638f57596be91afbf7e55ac8ae21a3de9e
Christopher Penkin <chris@penkin.net>
2017-03-21 23:14:30 +0200

    Added Google analytics and Discuss integration.

--- src/publishconf.py
+++ src/publishconf.py
@@ -19,6 +19,5 @@ CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
 DELETE_OUTPUT_DIRECTORY = True
 
 # Following items are often useful when publishing
-
-#DISQUS_SITENAME = ""
-#GOOGLE_ANALYTICS = ""
+DISQUS_SITENAME = "Penkin"
+GOOGLE_ANALYTICS = "UA-94073350-1"
