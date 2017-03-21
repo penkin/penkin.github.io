73083f14581e1da9a1ef710516b9948c4b39c97e
Christopher Penkin <chris@penkin.net>
2017-03-21 22:33:53 +0200

    Initial commit.

---
+++ content/hello.md
@@ -0,0 +1,17 @@
+Title: Hello World
+Date: 2017-03-21 21:57
+Modified: 2017-03-21 21:57
+Category: Writing
+Tags: writing, blog
+Slug: hello-world
+Authors: Christopher Penkin
+Summary: My obligatory hello post and why I'm here.
+
+Today I start a blog. Why? Well I want to become a better communicator. So to do that I'm
+going to put my thoughts on business, life, software development and everything in between
+right here; in this blog.
+
+I think it will be a great way to chronicle my journey in all aspects; maybe even write
+some how-tos so I don't forget them later?
+
+So **hello world**, see you all in the next article.
---
+++ pelicanconf.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Christopher Penkin'
+SITENAME = 'Penkin'
+SITESUBTITLE = 'Husband. Father. Software Developer.'
+SITEURL = ''
+
+MINT = True
+
+THEME = 'pelican-chunk'
+
+PATH = 'content'
+
+TIMEZONE = 'Africa/Johannesburg'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (
+    ('LinkedIn', 'https://linkedin.com/in/penkin/'),
+    ('Twitter', 'https://twitter.com/penkin'),
+    ('GitLab', 'https://gitlab.com/penkin'),
+    ('GitHub', 'https://github.com/penkin'),
+)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
